7f7aa20451bac0675e0b64a4f81b7e113e55528e
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-10-22 20:42:39 +0100

    add file

--- Kontroller/Windows/Vol.cs
+++ Kontroller/Windows/Vol.cs
@@ -1,7 +1,34 @@
-﻿namespace Kontroller.Windows
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Kontroller.Windows
 {
-    public class Vol
+    public static class Vol
     {
-        
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0x0a0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x090000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        public static Process p = Process.GetCurrentProcess();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        public static void Up()
+        {
+            SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+
+        public static void Down()
+        {
+            SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        public static void Mute()
+        {
+            SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
     }
 }
