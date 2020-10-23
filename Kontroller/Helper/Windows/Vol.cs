a79c509ddff8510144afff998cef077b7c30bc8b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-10-23 21:20:19 +0100

    Restrict Vol objs

--- Kontroller/Helper/Windows/Vol.cs
+++ Kontroller/Helper/Windows/Vol.cs
@@ -11,24 +11,24 @@ namespace Kontroller.Helper.Windows
         private const int APPCOMMAND_VOLUME_DOWN = 0x090000;
         private const int WM_APPCOMMAND = 0x319;
 
-        public static Process p = Process.GetCurrentProcess();
+        private static Process _p = Process.GetCurrentProcess();
 
         [DllImport("user32.dll")]
-        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+        private static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
         public static void Up()
         {
-            SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+            SendMessageW(_p.Handle, WM_APPCOMMAND, _p.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
         }
 
         public static void Down()
         {
-            SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+            SendMessageW(_p.Handle, WM_APPCOMMAND, _p.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
         }
 
         public static void Mute()
         {
-            SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+            SendMessageW(_p.Handle, WM_APPCOMMAND, _p.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
         }
     }
 }
