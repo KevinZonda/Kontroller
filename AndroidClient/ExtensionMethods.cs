31e63ecf7d3604fa0e2f93fcb8e2ce75049f200f
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-10-24 10:19:32 +0100

    Add Android base

---
+++ AndroidClient/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace AndroidClient
+{
+    public static class ExtensionMethods
+    {
+        public static bool StartsWith(this string s, params string[] prefixes)
+            => prefixes.Any(s.StartsWith);
+
+        public static bool Is(this string s, params string[] strings)
+            => strings.Any(ss => s == ss);
+    }
+}
--- AndroidClient/MainActivity.cs
+++ AndroidClient/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
@@ -9,11 +10,71 @@ namespace AndroidClient
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private EditText edtTarget;
+
+        private Button btnTest,
+            btnVolUp,
+            btnVolDown,
+            btnVolMute,
+            btnMediaPrev,
+            btnMediaPlay,
+            btnMediaNext;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            edtTarget = FindViewById<EditText>(Resource.Id.edtTarget);
+
+            btnTest = FindViewById<Button>(Resource.Id.btnTest);
+
+            btnVolUp = FindViewById<Button>(Resource.Id.btnVolUp);
+            btnVolDown = FindViewById<Button>(Resource.Id.btnVolDown);
+            btnVolMute = FindViewById<Button>(Resource.Id.btnVolMute);
+
+            btnMediaPrev = FindViewById<Button>(Resource.Id.btnMediaPrev);
+            btnMediaPlay = FindViewById<Button>(Resource.Id.btnMediaPlay);
+            btnMediaNext = FindViewById<Button>(Resource.Id.btnMediaNext);
+        }
+
+        private static string TidyUrl(string url)
+        {
+            url = url.ToLower().Trim();
+
+            string prefix = "http";
+            string suffix = string.Empty;
+            var v = url.Split("://");
+            switch (v.Length)
+            {
+                case 2:
+                    suffix = v[1];
+                    if (v[0].Is("http", "https"))
+                    {
+                        prefix = v[0];
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Scheme can be only http and https.\n" +
+                                                    "Yours is" + v[0]);
+                    }
+                    break;
+                case 1:
+                    if (v[0].Is("http", "https") && url.EndsWith("://"))
+                    {
+                        prefix = v[0];
+                    }
+                    else
+                    {
+                        suffix = v[0];
+                    }
+                    break;
+                default:
+                    return string.Empty;
+            }
+
+            suffix = suffix.Split("/")[0];
+            return prefix + "://" + suffix;
         }
     }
 }
