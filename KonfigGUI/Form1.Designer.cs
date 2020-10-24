54931a80c4cf40145d2359aae09c1c8edbba5822
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-10-24 21:15:30 +0100

    Throw useless event

--- KonfigGUI/Form1.Designer.cs
+++ KonfigGUI/Form1.Designer.cs
@@ -120,7 +120,6 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Kontroller Konfig GUI";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
--- KonfigGUI/Form1.cs
+++ KonfigGUI/Form1.cs
@@ -16,10 +16,5 @@ namespace KonfigGUI
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
